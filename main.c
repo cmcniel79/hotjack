7a01209b88ac1f4706dd938a8634d61d2ad61831
Chase <chase@fromthepeople.co>
2022-04-22 07:37:05 -0700

    All ultrasonic sensors working with drive motor

--- main.c
+++ main.c
@@ -18,7 +18,9 @@
 #define LEFT_ENCODER_PIN (P9_5)
 #define RIGHT_ENCODER_PIN (P9_6)
 #define DRIVE_MOTOR_PIN (P9_0)
-#define CENTER_ECHO_PIN (P10_2)
+#define LEFT_ECHO_PIN (P9_1)
+#define CENTER_ECHO_PIN (P9_2)
+#define RIGHT_ECHO_PIN (P9_3)
 
 /* Interrupt Priorities  */
 #define LEFT_ENCODER_PRIORITY (7u)
@@ -65,7 +67,10 @@ static void right_encoder_interrupt_handler(void *handler_arg, cyhal_gpio_event_
 static void calibration_btn_interrupt_handler(void *handler_arg, cyhal_gpio_event_t event);
 static void ultra_trig_handler(void);
 static void echo_timers_init(void);
+static void left_echo_handler(void *handler_arg, cyhal_gpio_event_t event);
 static void center_echo_handler(void *handler_arg, cyhal_gpio_event_t event);
+static void right_echo_handler(void *handler_arg, cyhal_gpio_event_t event);
+
 /*******************************************************************************
  * Global Variables
  ********************************************************************************/
@@ -89,11 +94,16 @@ cy_stc_sysint_t ISR_ultra_trig_config = {
     .intrPriority = TRIG_PRIORITY
 };
 
-/* Timer object used for recording Echo pulse time */
+/* Objects and variables used for ultrasonic sensors */
+cyhal_timer_t left_echo_timer;
+uint32_t left_echo_time;
+bool left_echo_is_pulsing;
 cyhal_timer_t center_echo_timer;
 uint32_t center_echo_time;
 bool center_echo_is_pulsing;
-
+cyhal_timer_t right_echo_timer;
+uint32_t right_echo_time;
+bool right_echo_is_pulsing;
 
 /*******************************************************************************
  * Function Name: main
@@ -115,77 +125,75 @@ int main(void)
     cy_retarget_io_init(CYBSP_DEBUG_UART_TX, CYBSP_DEBUG_UART_RX,
                                  CY_RETARGET_IO_BAUDRATE);
 
-    /* Initialize pin P0_5 for LEFT encoder*/
+    /*******************************************************************************
+    * Set up Encoder pins and their interrupts
+    ********************************************************************************/
+    /* ---- LEFT Encoder Pin Setup ---- */
     cyhal_gpio_init(LEFT_ENCODER_PIN, CYHAL_GPIO_DIR_INPUT,
                              CYHAL_GPIO_DRIVE_PULLUP, true);
-    /* Initialize pin P0_6 for RIGHT encoder*/
-    cyhal_gpio_init(RIGHT_ENCODER_PIN, CYHAL_GPIO_DIR_INPUT,
-                             CYHAL_GPIO_DRIVE_PULLUP, true);
-
-    /* Configure LEFT Encoder GPIO interrupt */
     cyhal_gpio_register_callback(LEFT_ENCODER_PIN,
                                  left_encoder_interrupt_handler, NULL);
-
-    /* Configure RIGHT Encoder GPIO interrupt */
-    cyhal_gpio_register_callback(RIGHT_ENCODER_PIN,
-                                 right_encoder_interrupt_handler, NULL);
-
-    /* Enable LEFT Encoder GPIO interrupt event when signal goes from HIGH to LOW*/
     cyhal_gpio_enable_event(LEFT_ENCODER_PIN, CYHAL_GPIO_IRQ_FALL,
                             LEFT_ENCODER_PRIORITY, true);
 
-    /* Enable RIGHT Encoder GPIO interrupt event when signal goes from HIGH to LOW*/
+    /* ---- RIGHT Encoder Pin Setup ---- */
+    cyhal_gpio_init(RIGHT_ENCODER_PIN, CYHAL_GPIO_DIR_INPUT,
+                             CYHAL_GPIO_DRIVE_PULLUP, true);
+    cyhal_gpio_register_callback(RIGHT_ENCODER_PIN,
+                                 right_encoder_interrupt_handler, NULL);
     cyhal_gpio_enable_event(RIGHT_ENCODER_PIN, CYHAL_GPIO_IRQ_FALL,
                             RIGHT_ENCODER_PRIORITY, true);
 
-    /* Configure SW2 Button for calibration */
+    /*******************************************************************************
+    * Set up SW2 button and interrupt for calibrating drive motor
+    ********************************************************************************/
     cyhal_gpio_init(CYBSP_USER_BTN, CYHAL_GPIO_DIR_INPUT,
                               CYHAL_GPIO_DRIVE_PULLUP, true);
-
-    /* Configure SW2 Button interrupt */
     cyhal_gpio_register_callback(CYBSP_USER_BTN,
                                  calibration_btn_interrupt_handler, NULL);
-
-    /* Enable SW2 Button interrupt event when signal goes from LOW to HIGH*/
     cyhal_gpio_enable_event(CYBSP_USER_BTN, CYHAL_GPIO_IRQ_FALL,
     						CAL_BUTTON_PRIORITY, true);
 
-    /* Initialize the Drive Motor PWM */
-    result = cyhal_pwm_init(&drive_motor_pwm, DRIVE_MOTOR_PIN, NULL);
-    if (CY_RSLT_SUCCESS != result)
-    {
-        printf("API cyhal_pwm_init failed with error code: %lu\r\n", (unsigned long)result);
-        CY_ASSERT(false);
-    }
-    /* Set the PWM period and pulsewidth */
-    result = cyhal_pwm_set_period(&drive_motor_pwm, PWM_PERIOD, drive_motor_input);
-    if (CY_RSLT_SUCCESS != result)
-    {
-        printf("API cyhal_pwm_set_duty_cycle failed with error code: %lu\r\n", (unsigned long)result);
-        CY_ASSERT(false);
-    }
-    result = cyhal_pwm_start(&drive_motor_pwm);
-    if (CY_RSLT_SUCCESS != result)
-    {
-        printf("API cyhal_pwm_start failed with error code: %lu\r\n", (unsigned long)result);
-        CY_ASSERT(false);
-    }
+    /*******************************************************************************
+    * Set up Drive Motor PWM 
+    ********************************************************************************/
+    cyhal_pwm_init(&drive_motor_pwm, DRIVE_MOTOR_PIN, NULL);
+    cyhal_pwm_set_period(&drive_motor_pwm, PWM_PERIOD, drive_motor_input);
+    cyhal_pwm_start(&drive_motor_pwm);
 
-    /* ----ECHO PINs SETUP---- */
-    /* Set up echo pin with interrupts */
-    result = cyhal_gpio_init(CENTER_ECHO_PIN, CYHAL_GPIO_DIR_INPUT,
+    /*******************************************************************************
+    * Set up pins for the ultrasonic echo measurements and their interrups
+    ********************************************************************************/
+    /* ---- LEFT Echo Pin Setup ---- */
+    cyhal_gpio_init(LEFT_ECHO_PIN, CYHAL_GPIO_DIR_INPUT,
                         CYHAL_GPIO_DRIVE_NONE, CYBSP_BTN_OFF);
+    cyhal_gpio_register_callback(LEFT_ECHO_PIN, left_echo_handler, NULL);
+    cyhal_gpio_enable_event(LEFT_ECHO_PIN, CYHAL_GPIO_IRQ_BOTH,
+                            ECHO_PRIORITY, true);
 
-    /* Set up inerrupt handler for the echo signal */
+    /* ---- CENTER Echo Pin Setup ---- */
+    cyhal_gpio_init(CENTER_ECHO_PIN, CYHAL_GPIO_DIR_INPUT,
+                        CYHAL_GPIO_DRIVE_NONE, CYBSP_BTN_OFF);
     cyhal_gpio_register_callback(CENTER_ECHO_PIN, center_echo_handler, NULL);
-
-    /* Set up interrupt for both rising and falling of the echo signal */
     cyhal_gpio_enable_event(CENTER_ECHO_PIN, CYHAL_GPIO_IRQ_BOTH,
                             ECHO_PRIORITY, true);
 
-    /* Initialize echo timer */
+    /* ---- RIGHT Echo Pin Setup ---- */
+    cyhal_gpio_init(RIGHT_ECHO_PIN, CYHAL_GPIO_DIR_INPUT,
+                        CYHAL_GPIO_DRIVE_NONE, CYBSP_BTN_OFF);
+    cyhal_gpio_register_callback(RIGHT_ECHO_PIN, right_echo_handler, NULL);
+    cyhal_gpio_enable_event(RIGHT_ECHO_PIN, CYHAL_GPIO_IRQ_BOTH,
+                            ECHO_PRIORITY, true);
+
+    /* Initialize ALL echo timers */
     echo_timers_init();
 
+    /*******************************************************************************
+    * Initialize PWM and interrupt for the trigger pulse for the ultrasonic sensors
+    * !!!! Important !!!!
+    * ---> This PWM is configured through the Device Congigurator, and that is 
+    *       why it uses the CY_TCPWM functions that are found in cy_pdl
+    ********************************************************************************/
     Cy_TCPWM_PWM_Init(TCPWM0, ultra_trig_pwm_NUM, &ultra_trig_pwm_config);
     Cy_TCPWM_PWM_Enable(TCPWM0, ultra_trig_pwm_NUM);
     Cy_TCPWM_SetInterruptMask(TCPWM0, ultra_trig_pwm_NUM, CY_TCPWM_INT_ON_TC);
@@ -193,14 +201,13 @@ int main(void)
     NVIC_EnableIRQ((IRQn_Type)ISR_ultra_trig_config.intrSrc);
     Cy_TCPWM_TriggerStart_Single(TCPWM0, ultra_trig_pwm_NUM);
 
-
     /* Enable global interrupts */
     __enable_irq();
 
     /* \x1b[2J\x1b[;H - ANSI ESC sequence for clear screen */
     printf("\x1b[2J\x1b[;H");
     printf("**************** PSoC 6 MCU: PID Testing *****************\r\n");
-    printf("Target: %5.3f\n\r", target_magnet_count);
+    printf("Target Magnet Count: %5.3f\n\r", target_magnet_count);
 
     /* Initialize timer for control loop */
     pid_timer_init();
@@ -213,7 +220,9 @@ int main(void)
     float kp = 1.0f;
     float ki = 0.05f;
 
-    float distance_cm = 0.0f;
+    float left_distance_cm = 0.0f;
+    float center_distance_cm = 0.0f;
+    float right_distance_cm = 0.0f;
 
     for (;;)
     {
@@ -243,10 +252,10 @@ int main(void)
                 drive_motor_input = PWM_PULSE_WIDTH_MIN_FORWARD;
             }
 
-            printf("Left: %5u\n\r", left_magnet_count);
-        	printf("Right: %5u\n\r", right_magnet_count);
-            printf("Error: %5i\n\r", error_sum);
-        	printf("Motor Input: %5u\n\r", drive_motor_input);
+            // printf("Left: %5u\n\r", left_magnet_count);
+        	// printf("Right: %5u\n\r", right_magnet_count);
+            // printf("Error: %5i\n\r", error_sum);
+        	// printf("Motor Input: %5u\n\r", drive_motor_input);
 
             /* Reset magnet counts */
             left_magnet_count = 0;
@@ -256,9 +265,17 @@ int main(void)
             pid_timer_flag = false;
         }
             
-        distance_cm = (SPEED_OF_SOUND_CM_PER_US * center_echo_time * 100);
-        if (distance_cm < 40.0) {
-            printf("Distance in: %3.1f cm \r\n", distance_cm);
+        left_distance_cm = (SPEED_OF_SOUND_CM_PER_US * left_echo_time * 25);
+        if (left_distance_cm < 40.0) {
+            printf("Left Distance in: %3.1f cm \r\n", left_distance_cm);
+        }
+        center_distance_cm = (SPEED_OF_SOUND_CM_PER_US * center_echo_time * 25);
+        if (center_distance_cm < 40.0) {
+            printf("Center Distance in: %3.1f cm \r\n", center_distance_cm);
+        }
+        right_distance_cm = (SPEED_OF_SOUND_CM_PER_US * right_echo_time * 25);
+        if (right_distance_cm < 40.0) {
+            printf("Right Distance in: %3.1f cm \r\n", right_distance_cm);
         }
     }
 }
@@ -287,9 +304,7 @@ static void calibration_btn_interrupt_handler(void *handler_arg, cyhal_gpio_irq_
 	drive_is_calibrated = true;
 }
 
-/*******************************************************************************
- * Function Name: pid_timer_init
- *******************************************************************************/
+/* Initialize PID timer */
 static void pid_timer_init(void)
 {
     cy_rslt_t result;
@@ -304,37 +319,16 @@ static void pid_timer_init(void)
             .value = 0                         /* Initial value of counter */
         };
 
-    /* Initialize the timer object. Does not use input pin ('pin' is NC) and
-     * does not use a pre-configured clock source ('clk' is NULL). */
-    result = cyhal_timer_init(&pid_timer, NC, NULL);
-
-    /* timer init failed. Stop program execution */
-    if (result != CY_RSLT_SUCCESS)
-    {
-        CY_ASSERT(0);
-    }
-
-    /* Configure timer period and operation mode such as count direction,
-       duration */
+    cyhal_timer_init(&pid_timer, NC, NULL);
     cyhal_timer_configure(&pid_timer, &pid_timer_cfg);
-
-    /* Set the frequency of timer's clock source */
     cyhal_timer_set_frequency(&pid_timer, ENCODER_TIMER_CLOCK_HZ);
-
-    /* Assign the ISR to execute on timer interrupt */
     cyhal_timer_register_callback(&pid_timer, pid_timer_handler, NULL);
-
-    /* Set the event on which timer interrupt occurs and enable it */
     cyhal_timer_enable_event(&pid_timer, CYHAL_TIMER_IRQ_TERMINAL_COUNT,
                              7, true);
-
-    /* Start the timer with the configured settings */
     cyhal_timer_start(&pid_timer);
 }
 
-/*******************************************************************************
- * Function Name: pid_timer_handler
- *******************************************************************************/
+/* Set pid timer flag to true when PID timer finishes count */
 static void pid_timer_handler(void *handler_arg, cyhal_gpio_event_t event)
 {
     (void)handler_arg;
@@ -344,18 +338,14 @@ static void pid_timer_handler(void *handler_arg, cyhal_gpio_event_t event)
     pid_timer_flag = true;
 }
 
-/*******************************************************************************
- * Function Name: ultra_trig_handler
- *******************************************************************************/
+/* Fires the pulse on the trigger pin to start measuring on ultrasonic sensors */
 static void ultra_trig_handler(void)
 {
     Cy_TCPWM_ClearInterrupt(ultra_trig_pwm_HW, ultra_trig_pwm_NUM, CY_TCPWM_INT_ON_TC);
     NVIC_ClearPendingIRQ(ISR_ultra_trig_config.intrSrc);
 }
 
-/*******************************************************************************
- * Function Name: echo_timers_init
- *******************************************************************************/
+/* Initialize all of the Echo timers */
 static void echo_timers_init(void)
 {
     cy_rslt_t result;
@@ -369,17 +359,38 @@ static void echo_timers_init(void)
             .is_continuous = true,          
             .value = 0                       
         };
+    /* Initialize all Echo timers with the same config */
 
-    result = cyhal_timer_init(&center_echo_timer, NC, NULL);
-
-    /* timer init failed. Stop program execution */
-    if (result != CY_RSLT_SUCCESS)
-    {
-        CY_ASSERT(0);
-    }
-
+    /* LEFT Echo Init */
+    cyhal_timer_init(&left_echo_timer, NC, NULL);
+    cyhal_timer_configure(&left_echo_timer, &echo_timer_cfg);
+    cyhal_timer_set_frequency(&left_echo_timer, ECHO_TIMER_CLOCK_HZ);
+    /* CENTER Echo Init */
+    cyhal_timer_init(&center_echo_timer, NC, NULL);
     cyhal_timer_configure(&center_echo_timer, &echo_timer_cfg);
     cyhal_timer_set_frequency(&center_echo_timer, ECHO_TIMER_CLOCK_HZ);
+    /* RIGHT Echo Init */
+    cyhal_timer_init(&right_echo_timer, NC, NULL);
+    cyhal_timer_configure(&right_echo_timer, &echo_timer_cfg);
+    cyhal_timer_set_frequency(&right_echo_timer, ECHO_TIMER_CLOCK_HZ);
+}
+
+/*******************************************************************************
+ * Function Name: left_echo_handler
+ *******************************************************************************/
+static void left_echo_handler(void *handler_arg, cyhal_gpio_irq_event_t event)
+{
+    (void)handler_arg;
+    (void)event;
+    if(left_echo_is_pulsing) {
+        left_echo_time = cyhal_timer_read(&left_echo_timer);
+        cyhal_timer_stop(&left_echo_timer);
+        left_echo_is_pulsing = false;
+    } else {
+        cyhal_timer_reset(&left_echo_timer);
+        cyhal_timer_start(&left_echo_timer);
+        left_echo_is_pulsing = true;
+    }
 }
 
 /*******************************************************************************
@@ -397,6 +408,24 @@ static void center_echo_handler(void *handler_arg, cyhal_gpio_irq_event_t event)
         cyhal_timer_reset(&center_echo_timer);
         cyhal_timer_start(&center_echo_timer);
         center_echo_is_pulsing = true;
+    }
+}
+
+/*******************************************************************************
+ * Function Name: right_echo_handler
+ *******************************************************************************/
+static void right_echo_handler(void *handler_arg, cyhal_gpio_irq_event_t event)
+{
+    (void)handler_arg;
+    (void)event;
+    if(right_echo_is_pulsing) {
+        right_echo_time = cyhal_timer_read(&right_echo_timer);
+        cyhal_timer_stop(&right_echo_timer);
+        right_echo_is_pulsing = false;
+    } else {
+        cyhal_timer_reset(&right_echo_timer);
+        cyhal_timer_start(&right_echo_timer);
+        right_echo_is_pulsing = true;
     }
 }
 
